46636e8d90be6c86d407f2e158956e11f1610e5c
rahulkhartude <rahulkhartude31@gmail.com>
2025-06-28 18:36:08 +0530

    cord added

--- Backend/db/config.js
+++ Backend/db/config.js
@@ -44,6 +44,6 @@
 const mongoose = require('mongoose');
 require('dotenv').config();
 
-mongoose.connect(process.env.MONGO_URL)
+mongoose.connect(process.env.MONGO_URI)
 .then(() => console.log('MongoDB connected'))
 .catch(err => console.error('MongoDB connection error:', err));

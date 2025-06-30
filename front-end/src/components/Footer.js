fcc055fa3c4f133027cbb2fd8032e4dbaa6a8b26
rahulkhartude <rahulkhartude31@gmail.com>
2025-07-01 00:35:31 +0530

    footer removed

--- front-end/src/components/Footer.js
+++ front-end/src/components/Footer.js
@@ -16,8 +16,10 @@ import React from 'react'
 
 export const Footer = () => {
   return (
-    <div className='footer'>
-      <h3>Footer</h3>
-    </div>
+    // <div className='footer'>
+    //   <h3>Footer</h3>
+    // </div>
+    <>
+    </>
   )
 }

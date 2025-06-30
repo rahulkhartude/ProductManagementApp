159d5ca9814aab03d02133abdc0c75a80da74fb6
rahulkhartude <rahulkhartude31@gmail.com>
2025-06-30 23:08:24 +0530

    cors changes

--- Backend/db/config.js
+++ Backend/db/config.js
@@ -51,7 +51,7 @@
 
 require('dotenv').config();
 const mongoose = require('mongoose');
-
+console.log('MONGO_URI is:', process.env.MONGO_URI);
 if (!process.env.MONGO_URI) {
   console.error('MONGO_URI is not defined in .env file');
   process.exit(1);
